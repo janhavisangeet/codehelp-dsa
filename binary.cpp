a1e09a8e379a0bb0a9939113d47e94e248281ec3
hariharan <animatrix535@gmail.com>
2023-07-31 13:54:43 +0530

    Initial commit

---
+++ binary.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+
+int main(){
+      
+      int n;
+      cin>>n;
+
+     int i=0, ans=0;
+
+      while(n!=0){
+
+        int bit=n&1;
+
+        if(bit==1){
+            ans=ans+pow(2,i);
+        }
+        n=n>>1;
+        i++;
+      }
+cout<<ans<<endl;
+
+}
+
