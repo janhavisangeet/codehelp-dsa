f84199aa25b0ab2ce44470f1f45cc8e79c6a6f82
hariharan <animatrix535@gmail.com>
2023-08-03 13:49:10 +0530

    make the changes

--- binary.cpp
+++ binary.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<math.h>
+using namespace std;
 
 int main(){
       
@@ -7,19 +8,18 @@ int main(){
       cin>>n;
 
      int i=0, ans=0 ;
-     int temp = 0;
-
+     
       while(n!=0){
 
-        int bit=n&1;
+        int digit= n % 10;
 
-        if(bit==1){
+        if(digit==1){
             ans=ans+pow(2,i);
         }
-        n=n>>1;
+        n=n/10;
         i++;
       }
-cout<<ans<<endl;
+      cout << ans << endl;
 
 }
 
