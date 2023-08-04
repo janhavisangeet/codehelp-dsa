53cb9f32be0105e5984b80ab6b4b22420aad3c96
hariharan <animatrix535@gmail.com>
2023-08-04 18:59:51 +0530

    make change

---
+++ Arrays.cpp/scope.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void update(int arr[], int n){
+    
+  cout<<"INside the function"<<endl;
+
+  //updating aaray's first element
+  arr[0]=120;
+
+//printing the array
+for(int i=0; i<3; i++){
+    cout<<arr[i]<<" ";
+}
+cout<<endl;
+
+  cout<<"Going back to main function"<<endl;
+    
+}
+
+int main(){
+ 
+  int arr[3]={1,2,3};
+
+  update(arr,3);
+
+//printing the array
+cout<<"printing in the min function"<<endl;
+for(int i=0; i<3; i++){
+    cout<<arr[i]<<" ";
+}
+cout<<endl;
+
+return 0;
+
+}
